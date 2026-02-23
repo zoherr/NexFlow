213fa63c551db929c1d14017e90f1d1634206c96
zoherr <workforzoher@gmail.com>
2026-02-23 07:55:38 +0530

    add logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
+import { Button } from "@/components/ui/button";
+import LogoutButton from "@/features/auth/components/LogoutButton";
 import { requireAuth } from "@/lib/auth-utils"
+import { caller } from "@/trpc/server";
 
 const Page = async () => {
 
   const session = await requireAuth();
 
+  const data = await caller.getUsers();
   return (
     <div className='min-h-screen flex flex-col px-4'>
       {session && <>
-      logged in
+        logged in
       </>}
+      {
+        JSON.stringify(data)
+      }
+      <LogoutButton />
     </div>
   )
 }
